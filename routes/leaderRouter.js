2dc8b13ecf08e668a9655266bdb6702deaa041bf
sanjeevsky <knit16643@gmail.com>
2020-04-29 14:43:44 +0530

    first Commit

---
+++ routes/leaderRouter.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var leaderRouter  = express.Router();
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type','Text/Plain');
+  next();
+})
+
+.get((req,res,next) => {
+  res.end('Will send all the leaders to you!');
+})
+
+.post((req,res,next) => {
+  res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);    
+})
+
+.put((req,res,next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /leaders');
+})
+
+.delete((req,res,next) => {
+  res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type','Text/Plain');
+  next();
+})
+
+.get((req,res,next) => {
+  res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+
+.post((req,res,next) => {
+  res.statusCode = 403;
+    res.end('post operation not supported on leaders/:'+leaderId);
+})
+
+.put((req,res,next) => {
+  res.end('Updating the leader: ' + req.params.leaderId + '\n');
+})
+
+.delete((req,res,next) => {
+  res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+module.exports = leaderRouter;
